88948355abc2209c0fad98c142db164ddd950fff
Vinicius Maggi <viniciusmaggi@outlook.com>
2020-02-11 02:38:23 -0300

    update full system

---
+++ login_system/conexao.php
@@ -0,0 +1,11 @@
+<?php
+define('HOST', 'mysql669.umbler.com');
+define('USUARIO', 'attrative');
+define('SENHA', 'admin123admin');
+define('DB', 'attrative');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+
+//comando para funcionar conexão com o server
+//ALTER USER root@'localhost' IDENTIFIED WITH mysql_native_password BY 'adminvmo'
+//ALTER USER Canal@mysql669.umbler.com IDENTIFIED WITH mysql_native_password BY 'admin123admin'
